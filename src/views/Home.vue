457e3dd8951c1266a4f5966866baf48439d70dee
levy_admin <marlonlevy@gmail.com>
2026-04-24 13:02:54 -0400

    Nodes page updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,10 @@
   <v-container fluid>
     <v-row>
       <v-col>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is a simple home page for the Chaos App.</p>
+        <h1>Nodes</h1>
         <div>{{ getNodes.length }} nodes available</div>
-        <p>{{ getNodes }}</p>
+
+        <v-data-table :items="getNodes" :headers="nodeHeaders"> </v-data-table>
       </v-col>
     </v-row>
   </v-container>
@@ -17,6 +17,19 @@ import { storeToRefs } from 'pinia'
 
 const resourceStore = useResourceStore()
 const { getNodes } = storeToRefs(resourceStore)
+const nodeHeaders = [
+  { title: 'Name', value: 'name' },
+  { title: 'Status', value: 'status' },
+  { title: 'Roles', value: 'roles' },
+  { title: 'Age', value: 'age' },
+  { title: 'Version', value: 'version' },
+  { title: 'Version', value: 'version' },
+  { title: 'Internal-IP', value: 'internal_ip' },
+  { title: 'External-IP', value: 'external_ip' },
+  { title: 'OS-Image', value: 'os_image' },
+  { title: 'Kernel-Version', value: 'kernel_version' },
+  { title: 'Container-Runtime', value: 'container_runtime' },
+]
 
 onMounted(async () => {
   console.log('Home component mounted')
